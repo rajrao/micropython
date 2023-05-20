d4482b0e97b7f3dd2bae07a9e06a6ed80a8b04e0
Raj <raj.m.rao@gmail.com>
2023-05-19 22:19:27 -0600

    Create utils-clear-console.py

---
+++ pybricks/utils-clear-console.py
@@ -0,0 +1,13 @@
+print("\x1b[H\x1b[2J", end="") #this will clear the console screen
+
+print("hello")
+print("world")
+
+wait(1000)
+
+print("\x1b[H\x1b[2J", end="")
+
+wait(1000)
+
+print("team electrosaurus")
+print("rules!")
